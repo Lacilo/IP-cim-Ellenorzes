26a5ef7c460dc0706833dce21e862531ac4bd9a5
HIAndris <144882697+HIAndris@users.noreply.github.com>
2024-12-16 16:59:34 +0100

    Gyógyítás (IPv6 rövidített formátum)

--- modules/iptype.py
+++ modules/iptype.py
@@ -1,7 +1,7 @@
 def ipType(ip):
     if len(ip.split(".")) == 4:
         return "IPv4"
-    elif len(ip.split(":")) == 8:
+    elif 1 < len(ip.split(":")) <= 8:
         return "IPv6"
     else:
         return "helytelen"
