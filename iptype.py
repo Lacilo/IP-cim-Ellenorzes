cb5fdcb573e0992c8711c6523b19f7435ca56f15
HIAndris <144882697+HIAndris@users.noreply.github.com>
2024-12-16 07:46:14 +0100

    Típusellenőrzés modul

---
+++ iptype.py
@@ -0,0 +1,9 @@
+def ipType(ip):
+    if len(ip.split(".")) == 4:
+        return "IPv4"
+    elif len(ip.split(":")) == 8:
+        return "IPv6"
+    else:
+        return "helytelen"
+
+print(ipType(input()))
