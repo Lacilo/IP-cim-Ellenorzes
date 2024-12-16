0830e0d5cc8917abe4150c07878552e24f088423
Lacilo <144881417+Lacilo@users.noreply.github.com>
2024-12-16 16:49:43 +0100

    Update ipvalid.py

--- modules/ipvalid.py
+++ modules/ipvalid.py
@@ -2,25 +2,48 @@ def check(cim):
     rovidites_sz = 0
     helyes = True
 
-    if len(cim) <= 15:
+    if "." in cim:
         ip_elemek = cim.split(".")
         for elem in ip_elemek:
-            if not 0 < int(elem) < 256 or len(ip_elemek) != 4:
+            try:
+                if elem[0] == "0" and len(elem) > 1:
+                    helyes = False
+                elem = int(elem)
+
+                if not -1 < elem < 256 or len(ip_elemek) != 4:
+                    helyes = False
+            except:
                 helyes = False
-    else:
+            
+    elif ":" in cim:
         ip_elemek = cim.split(":")
         for elem in ip_elemek:
-            if elem == "":
-                rovidites_sz += 1
-            elif not -1 < int(elem, 16) < 65536 and (len(ip_elemek) != 8 or rovidites_sz == 1):
+            try:
+                if len(elem) > 4:
+                    helyes = False
+                if ip_elemek[0] == "" and ip_elemek[1] == "":
+                    ip_elemek[0] = "0"
+                    ip_elemek[1] = "0"
+                    ip_elemek[2] = "0"
+                if elem == "":
+                    rovidites_sz += 1
+                    elem = "0"
+                elem = int(elem, 16)
+                ip_elemek = cim.split(":")
+                if not -1 < elem < 65536 and (len(ip_elemek) != 8 or rovidites_sz == 1) or len(ip_elemek) > 8:
+                    helyes = False
+                elif ip_elemek[-1] == "" and ip_elemek[-2] != "":
+                    helyes = False
+                
+            except ValueError:
                 helyes = False
-                print(int(elem, 16))
-            print(len(ip_elemek))
 
-    print(ip_elemek)
-    
+    else:
+        return False
+            
     if rovidites_sz <= 1:
         return helyes
+    
 
 cim = input("kérem az ip címet --> ")
 
