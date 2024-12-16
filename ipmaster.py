eca1b798f25423a465e9d64ff4d8745cf5cbfa24
HIAndris <144882697+HIAndris@users.noreply.github.com>
2024-12-16 17:07:11 +0100

    Gyógyítás 2

--- ipmaster.py
+++ ipmaster.py
@@ -3,12 +3,12 @@ from modules import ipvalid
 
 ip = input("Adjon meg egy IP-címet: ")
 if iptype.ipType(ip) == "IPv4":
-    if ipvalid.ellenorzes(ip):
+    if ipvalid.check(ip):
         print("Ez egy IPv4-es cím.")
     else:
         print("Ez hasonlít egy IPv4-es címre, de nem helyes formátumú!")
 elif iptype.ipType(ip) == "IPv6":
-    if ipvalid.ellenorzes(ip):
+    if ipvalid.check(ip):
         print("Ez egy IPv6-os cím.")
     else:
         print("Ez hasonlít egy IPv6-os címre, de nem helyes formátumú!")
